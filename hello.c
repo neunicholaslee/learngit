deb3a882a020216fb899e1560b226a2faeefb324
zdl <zhengdali2005@163.com>
2014-06-10 14:36:55 +0800

    add {}

--- hello.c
+++ hello.c
@@ -3,7 +3,9 @@ int main(int argc, char *argv[ ])
 {
     printf("%d\n",argc);
     while(argc)
-    printf("%s\n",argv[--argc]);
+    {
+        printf("%s\n",argv[--argc]);
+    }
     return 0;
 
 }
